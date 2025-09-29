c235e3e0caa4709e81f78ae99e94e11f5ca8d1bc
sahilmulla16 <sahilmulla9152@gmail.com>
2025-09-29 12:51:24 +0530

    Model Changed and Datastorage added for RAG

--- globals.py
+++ globals.py
@@ -10,13 +10,13 @@ from langchain_chroma import Chroma
 
 dotenv.load_dotenv()
 
-LLM_MODEL = os.environ.get("LLM_MODEL", "llama-4-scout-17b-16e-instruct")
+LLM_MODEL = os.environ.get("LLM_MODEL", "Llama-3.3-70B")
 TEMPERATURE = float(os.environ.get("LLM_TEMPERATURE", "0"))
 MAX_REFLECTIONS = int(os.environ.get("MAX_REFLECTIONS", "2"))
 MEM_COLLECTION = os.environ.get("MEM_COLLECTION", "mini_manus_memory")
 EMBED_MODEL = os.environ.get("EMBED_MODEL", "all-MiniLM-L6-v2")
-PERSIST_DIR = "/kaggle/working/agent_memory"
-RAG_PERSIST_DIR = "/kaggle/working/"
+PERSIST_DIR = "./agent_memory"
+RAG_PERSIST_DIR = "./rag_docs"
 RAG_COLLECTION = "rag_docs"
 
 _llm = ChatCerebras(
